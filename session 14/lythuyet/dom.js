8b1688ff85f54b77f2f9eb1688c63a240ecbd8df
chien2k60609-sys <chien2k60609@gmail.com>
2026-03-06 05:51:11 +0700

    Add files via upload

---
+++ session 14/lythuyet/dom.js
@@ -0,0 +1,14 @@
+
+/*
+DOM: DOCUMENT OBJECT MODEL : MÔ HÌNH TÀI LIỆU DẠNG ĐỐI TƯỢNG
+AI TẠO RA DOM? ==> khi trang web được tải lên thì trình duyệt sẽ tạo ra mô hình cây dom
+Dựa vào mô hình DOM này chúng ta có thể làm?
+    + thay đổi nội dung trang web
+    + thay đổi các phần tử, thuộc tính, ... của các thẻ HTML
+    + tương tác các sự kiện trong phần tử HTML
+CÁC THÀNH PHẦN TRONG DOM
+1. ELEMENT
+2. ATRIBUTE (THUỘC TÍNH)
+3. TEXT NỘI DUNG
+
+*/
